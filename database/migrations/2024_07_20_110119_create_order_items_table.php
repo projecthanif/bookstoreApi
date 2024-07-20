058bedecc8c03ce44fbeb04d5140b6f768ea6c8c
projecthanif <iamustapha213@gmail.com>
2024-07-20 12:37:27 +0100

    :created the migration tables for foundation of the bookstore api

---
+++ database/migrations/2024_07_20_110119_create_order_items_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained('orders')->restrictOnDelete();
+            $table->foreignId('book_id')->constrained('books')->restrictOnDelete();
+            $table->unsignedInteger('quantity');
+            $table->double('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_items');
+    }
+};
